2b3fb3848a5f668b15580f38fc8aa7b5ec291d00
SakethKumar <sakethamaragani043@gmail.com>
2021-09-04 23:42:06 +0530

    footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ReferencesComponent } from './references/references.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ReferencesComponent } from './references/references.component';
     ProjectsComponent,
     HeaderComponent,
     HomeComponent,
-    ReferencesComponent
+    ReferencesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,42 +7,7 @@ import { ViewportScroller } from '@angular/common';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
-  constructor(private viewportScroller : ViewportScroller) { }
-
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    let element = document.querySelector('.navbar') as HTMLElement;
-    if (window.pageYOffset > element.clientHeight) {
-      element.classList.add('bg-color');
-    } else {
-      element.classList.remove('bg-color');
-    }
-  }
- 
-
-  onClickScroll(elementId: string){
-    this.viewportScroller.scrollToAnchor(elementId);
-    var mybutton = document.getElementById("back-to-top");
-window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if(mybutton!=null){
-        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
-    }
-}
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-  }
-  }
-
+  
   
 
 }
